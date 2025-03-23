efb986f7d8b964d7f5dd713b3687e4f5451f8413
Fritz <fritzshaio@gmail.com>
2025-03-23 10:19:26 +0800

    Initial Commit

---
+++ TinTint/TinTintApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinTintApp.swift
+//  TinTint
+//
+//  Created by Fritz Hsiao on 2025/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TinTintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinTintTests/TinTintTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinTintTests.swift
+//  TinTintTests
+//
+//  Created by Fritz Hsiao on 2025/3/23.
+//
+
+import Testing
+@testable import TinTint
+
+struct TinTintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
